fe90d5a0d4b5f11cb9ad3f1f391776ef337ebd46
Monica Parente <mon8806@gmail.com>
2019-07-12 23:12:17 -0700

    finished burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Katsu Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cripsy Chicken Burger", true);
